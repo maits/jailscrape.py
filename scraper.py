055e7ff2500148549570eee911e5a20332fe9dad
Maite Fernandez <maite.fern@gmail.com>
2014-11-23 21:06:54 -0500

    Create scraper.py

---
+++ scraper.py
@@ -0,0 +1,48 @@
+import urllib2
+import csv
+from BeautifulSoup import BeautifulSoup
+
+url = 'http://www.showmeboone.com/sheriff/JailResidents/JailResidents.asp'
+
+# Open the HTML file and turn it into a BeautifulSoup object for parsing
+html = urllib2.urlopen(url).read()
+soup = BeautifulSoup(html)
+
+# The scrape actually starts here. Let's get the table that contains the results.
+only_table = soup.find('table', attrs={'class': 'resultsTable'})
+
+output_trs = [] # The list that's going to store all of our output rows
+
+# First we need to loop through all the rows in the table - first tr is a made-up var, second tr is the one in the html
+for tr in only_table.findAll('tr'):
+
+    # And next, we want to get all the cells within each of the rows
+    tds = tr.findAll('td')
+
+    # We'll store all of the values for each given row in a list
+    output_tds = []
+    print "********"
+    print tr
+    print "********"
+
+
+    for td in tds:
+        output_tds.append(td.text.replace('&nbsp;', '')) # Delete annoying tab character
+
+        print td
+
+
+    # And we'll add that list to our broader list of results
+    output_trs.append(output_tds)
+
+import sys
+sys.exit()
+
+# Finally, we'll write our results to a file
+
+#print output_trs
+
+handle = open('out.csv', 'a')
+outfile = csv.writer(handle)
+
+outfile.writerows(output_trs)
